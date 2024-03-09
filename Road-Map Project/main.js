f592f4ad4d217448bf1298c5be8d1c955f03a3fe
1030EswaranJ <161800689+1030EswaranJ@users.noreply.github.com>
2024-03-09 21:13:09 +0530

    Add files via upload

--- Road-Map Project/main.js
+++ Road-Map Project/main.js
@@ -1,3 +1,60 @@
 function home() {
     location.href = "./home.html";
-}
+  }
+
+  function undo(){
+    location.reload()
+  }
+
+  var html = document.getElementById("html"); 
+  var css = document.getElementById("css");
+  var js = document.getElementById("js");
+  var git = document.getElementById("git");
+  displaying = 1;
+
+  
+  function html1(){
+    
+    if(displaying === 1){
+      html.style.display = "block";
+      displaying = 0
+    }else{
+      html.style.display = "none";
+      displaying = 1
+    }
+  }
+
+  function css2(){
+    
+    if(displaying === 1){
+      css.style.display = "block";
+      displaying = 0
+    }else{
+      css.style.display = "none";
+      displaying = 1
+    }
+  }
+
+  function js3(){
+    
+    if(displaying === 1){
+      js.style.display = "block";
+      displaying = 0
+    }else{
+      js.style.display = "none";
+      displaying = 1
+    }
+  }
+
+  function git1(){
+    
+    if(displaying === 1){
+      git.style.display = "block";
+      displaying = 0
+    }else{
+      git.style.display = "none";
+      displaying = 1
+    }
+  }
+
+
