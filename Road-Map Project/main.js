07d7a1ad09ab87232ae2a7a373fd28eafabf5e38
1030EswaranJ <161800689+1030EswaranJ@users.noreply.github.com>
2024-03-10 21:19:22 +0530

    Add files via upload

--- Road-Map Project/main.js
+++ Road-Map Project/main.js
@@ -1,60 +1,142 @@
 function home() {
-    location.href = "./home.html";
+  location.href = "./home.html";
+}
+
+function undo() {
+  location.reload();
+}
+
+
+displaying = 1;
+
+var html = document.getElementById("html");
+function html1() {
+
+  if (displaying === 1) {
+    html.style.display = "block";
+    displaying = 0
+  } else {
+    html.style.display = "none";
+    displaying = 1
   }
+}
 
-  function undo(){
-    location.reload()
+var css = document.getElementById("css");
+function css1() {
+
+  if (displaying === 1) {
+    css.style.display = "block";
+    displaying = 0
+  } else {
+    css.style.display = "none";
+    displaying = 1
   }
+}
 
-  var html = document.getElementById("html"); 
-  var css = document.getElementById("css");
-  var js = document.getElementById("js");
-  var git = document.getElementById("git");
-  displaying = 1;
+var js = document.getElementById("js");
+function js1() {
 
-  
-  function html1(){
-    
-    if(displaying === 1){
-      html.style.display = "block";
-      displaying = 0
-    }else{
-      html.style.display = "none";
-      displaying = 1
-    }
+  if (displaying === 1) {
+    js.style.display = "block";
+    displaying = 0
+  } else {
+    js.style.display = "none";
+    displaying = 1
   }
+}
 
-  function css2(){
-    
-    if(displaying === 1){
-      css.style.display = "block";
-      displaying = 0
-    }else{
-      css.style.display = "none";
-      displaying = 1
-    }
+var git = document.getElementById("git");
+function git1() {
+  if (displaying === 1) {
+    git.style.display = "block";
+    displaying = 0
+  } else {
+    git.style.display = "none";
+    displaying = 1
   }
+}
 
-  function js3(){
-    
-    if(displaying === 1){
-      js.style.display = "block";
-      displaying = 0
-    }else{
-      js.style.display = "none";
-      displaying = 1
-    }
+var npm = document.getElementById("npm");
+function npm1() {
+
+  if (displaying === 1) {
+    npm.style.display = "block";
+    displaying = 0
+  } else {
+    npm.style.display = "none";
+    displaying = 1
   }
+}
 
-  function git1(){
-    
-    if(displaying === 1){
-      git.style.display = "block";
-      displaying = 0
-    }else{
-      git.style.display = "none";
-      displaying = 1
-    }
+var sass = document.getElementById("sass");
+function sass1() {
+
+  if (displaying === 1) {
+    sass.style.display = "block";
+    displaying = 0
+  } else {
+    sass.style.display = "none";
+    displaying = 1
   }
+}
+
+var bs = document.getElementById("bs");
+function bs1() {
+
+  if (displaying === 1) {
+    bs.style.display = "block";
+    displaying = 0
+  } else {
+    bs.style.display = "none";
+    displaying = 1
+  }
+}
+
+var rj = document.getElementById("rj");
+function rj1() {
+
+  if (displaying === 1) {
+    rj.style.display = "block";
+    displaying = 0
+  } else {
+    rj.style.display = "none";
+    displaying = 1
+  }
+}
+
+var inter = document.getElementById("inter");
+function inter1() {
+
+  if (displaying === 1) {
+    inter.style.display = "block";
+    displaying = 0
+  } else {
+    inter.style.display = "none";
+    displaying = 1
+  }
+}
 
 
+var auth = document.getElementById("auth");
+function auth1() {
+
+  if (displaying === 1) {
+    auth.style.display = "block";
+    displaying = 0
+  } else {
+    auth.style.display = "none";
+    displaying = 1
+  }
+}
+
+var sec = document.getElementById("sec");
+function sec1() {
+
+  if (displaying === 1) {
+    sec.style.display = "block";
+    displaying = 0
+  } else {
+    sec.style.display = "none";
+    displaying = 1
+  }
+}
