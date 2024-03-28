103ce7e5c16a32ed4830130a8c812af9137b4796
1030EswaranJ <161800689+1030EswaranJ@users.noreply.github.com>
2024-03-28 21:57:44 +0530

    Add files via upload

--- Road-Map Project/main.js
+++ Road-Map Project/main.js
@@ -2,6 +2,9 @@ function home() {
   location.href = "./home.html";
 }
 
+let cont = document.getElementsByClassName("content");
+console.log(cont);
+
 function undo(event) {
   let block = document.getElementById(event);
   block.style.display = "none";
@@ -9,9 +12,124 @@ function undo(event) {
 
 function content(con) {
   let a = document.getElementById(con);
-  if (a.style.display == "" || "none") {
-    a.style.display = "block";
-  } else {
+  if (a.style.display == "block") {
     a.style.display = "none";
+  } else {
+    a.style.display = "block";
   }
+}
+
+function menu(a) {
+  let menu = document.getElementById(a);
+
+  if (menu.style.display === "block") {
+    menu.style.display = "none";
+  } else {
+    menu.style.display = "block";
+  }
+}
+
+function fullStack(f, b) {
+  let febe = {
+    front: document.getElementById(f),
+    back: document.getElementById(b),
+  };
+
+  febe.front.style.border = "4px solid  #63f542";
+  febe.back.style.border = "4px solid #26ff35";
+}
+
+// ----------------------------------------------------------------------------------------------------
+
+function handleSubmit(event) {
+  event.preventDefault(); // Prevent default form submission
+  // Access form data
+  const formData = new FormData(event.target);
+
+  // Convert form data to JSON object
+  const user = {
+    id: formData.get("id"),
+    username: formData.get("username"),
+    email: formData.get("email"),
+    password: btoa(formData.get("password")),
+    confirmPassword: btoa(formData.get("c_password")),
+  };
+
+  console.log(user);
+
+  if (user.password === user.confirmPassword) {
+    console.log("matching");
+  } else {
+    console.log("Not Matching");
+    alert("Password and Confirm Password Not Matching");
+    process.exit();
+  }
+
+  // Make POST request with fetch API
+  fetch("http://localhost:8080/createUser", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Parse response body as JSON
+    })
+    .then((data) => {
+      console.log("Response from server:", data);
+      if (data.statusCode === 200) {
+        window.location.href = "./index.html";
+      } else {
+        emailError.style.display = "block";
+      }
+      // Handle success response as needed
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle error as needed
+    });
+}
+
+function handleLogin(event) {
+  event.preventDefault(); // Prevent default form submission
+  const formData = new FormData(event.target);
+
+  // Convert form data to JSON object
+  const user = {
+    email: formData.get("email"),
+    password: btoa(formData.get("password")),
+  };
+  fetch("http://localhost:8080/login", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(user),
+  })
+    .then((response) => {
+      console.log(response);
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json(); // Parse response body as JSON
+    })
+    .then((data) => {
+      console.log("Response from server:", data);
+      if (data.statusCode === 200) {
+        emailError.style.display = "none";
+        window.location.href = "./home.html";
+      } else {
+        emailError.style.display = "block";
+      }
+      // Handle success response as needed
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+      // Handle error as needed
+    });
 }
